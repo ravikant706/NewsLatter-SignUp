24b033e51c227880b55cb0aae6ee6806b7eb1c47
ravikant706 <ravikant706@gmail.com>
2023-10-29 08:56:46 +0530

    add logo and fix server problem and singup box desing fix and connect style.css to html main file

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = requrie("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
 
 const app = express();
+
+app.get("/", function(req, res){
+    res.send(__dirname + "/signup.html")
+})
 
 app.listen(3000, function () { 
     console.log("Server is running on port 3000");
